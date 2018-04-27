3778b89f27132ac7062c6abe4b100fdcd007fbd1
lab-ikoon <info@ikoon.cz>
2018-04-27 02:49:38 +0200

    qq

--- utils/wf_utils.py
+++ utils/wf_utils.py
@@ -2,7 +2,7 @@ from shutil import copyfile
 
 
 root       = "C:/Users/info/OneDrive/Documents/houdini16.5/"
-git        = "qq/"
+git        = "_git/qq/"
 
 src_py     = "python2.7libs/"
 src_readme = "python2.7libs/" 
@@ -25,8 +25,24 @@ files.append ("utils")
 
 
 for file in files :
+
     # py
     src = root + src_py + "wf_" + file + ".py"
-    dst = root + git + file + "/" + file + ".py"
+    dst = root + git + file + "/wf_" + file + ".py"
+    if file == "network_ui_ramp_lib" :
+        dst = root + git + "network_ui/wf_" + file + ".py"
+    copyfile(src, dst)
 
+    # readme
+    src = root + src_readme + "wf_" + file + ".md"
+    dst = root + git + file + "/readme.md"
+    if file == "network_ui_ramp_lib" :
+        dst = root + git + "network_ui/readme_ramp_lib.md"
+    copyfile(src, dst)
+
+    # shelf
+    if file == "network_ui_ramp_lib" :
+        file = "network_ui"
+    src = root + src_shelf + "wf_" + file + ".shelf"
+    dst = root + git + file + "/wf_" + file + ".shelf"
     copyfile(src, dst)
