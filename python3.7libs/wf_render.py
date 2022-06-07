473025262f4f2ca456288f2114f824d9f50d15f6
Jiri Miratsky <info@ikoon.cz>
2022-06-07 18:07:37 +0200

    lopnet automate hotkeys

--- python3.7libs/wf_render.py
+++ python3.7libs/wf_render.py
@@ -1,7 +1,5 @@
 import hou
-
-
-
+import nodesearch
 
 
 # create .py script for each selected ROP, to render it
@@ -46,18 +44,6 @@ def batch_script_rop () :
 
 
 
-def start () :
-    sceneview_main = hou.ui.curDesktop().findPaneTab("pt_sceneview_1")
-    # if sceneview_main.currentState() == "karma" :
-    try:
-        if sceneview_main.currentHydraRenderer() == "Karma" :
-            sceneview_main.restartRenderer()
-        else :
-            sceneview_main.setHydraRenderer("Karma")
-    except:
-        pass
-
-
 def kill () :
     sceneview_main = hou.ui.curDesktop().findPaneTab("pt_sceneview_1")
     # if sceneview_main.currentState() == "karma" :
@@ -67,3 +53,63 @@ def kill () :
     except:
         pass
 
+
+
+
+def start () :
+
+    sceneview_main = hou.ui.curDesktop().findPaneTab("pt_sceneview_1")
+    sceneview_edit = hou.ui.curDesktop().findPaneTab("pt_sceneview_2")
+
+    pe_1 = hou.ui.curDesktop().findPaneTab("pt_parmeditor_1")
+    nw_1 = hou.ui.curDesktop().findPaneTab("pt_network_1")
+                     
+    # find active lopnet
+    #-----------------------------------------------
+    root          = hou.node("/obj")
+    matcher       = nodesearch.NodeType( "lopnet", exact=True )
+    lopnets       = matcher.nodes( root, recursive=False )
+    lopnet_active = lopnets[0]
+
+    for lopnet in lopnets :
+        if lopnet.userData("lopnet_active") == "on":
+            lopnet_active = lopnet
+
+
+    # lock
+    #-----------------------------------------------
+    sceneview_main.setLinkGroup(hou.paneLinkType.Pinned)
+    sceneview_edit.setLinkGroup(hou.paneLinkType.Pinned)
+    pe_1.setLinkGroup(hou.paneLinkType.Pinned)
+    nw_1.setLinkGroup(hou.paneLinkType.Pinned)
+
+    # set pwd
+    #-----------------------------------------------
+    sceneview_main.setPwd(lopnet_active)
+    hou.hscript("viewcamera -c /cam  wf_desktop.pt_sceneview_1.solaris.persp1")
+
+
+    # unlock edit
+    #-----------------------------------------------
+    pe_1.setLinkGroup(hou.paneLinkType.FollowSelection)
+    # nw_1.setLinkGroup(hou.paneLinkType.FollowSelection)
+    sceneview_edit.setLinkGroup(hou.paneLinkType.FollowSelection)
+
+    # set camera
+    #-----------------------------------------------
+    # this is a bug
+    # lopnet_camera = lopnet_active.parm("force_cam").evalAsNode()
+    # sceneview_main.curViewport().setCamera(lopnet_camera)
+    # wf_desktop.pt_sceneview_1.solaris
+
+
+
+    # restart Karma render (or set Karma as renderer)
+    #-----------------------------------------------
+    try:
+        if sceneview_main.currentHydraRenderer() == "Karma" :
+            sceneview_main.restartRenderer()
+        else :
+            sceneview_main.setHydraRenderer("Karma")
+    except:
+        pass
